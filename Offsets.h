24adba8f360c4b209a475bb71fdd04db6b6bf045
Kryptic-0x69 <imsoscaredstop@gmail.com>
2025-02-02 15:42:07 -0500

    Created Offsets.h

---
+++ Offsets.h
@@ -0,0 +1,35 @@
+// Fortnites Latest Update ++Fortnite+Release-33.30-CL-39435251-Windows
+// Date Of Update: 02/01/2025
+
+#define UWORLD 0x16FB0098
+#define GAME_INSTANCE 0x200
+#define LOCAL_PLAYERS 0x38
+#define PLAYER_CONTROLLER 0x30
+#define LOCAL_PAWN 0x350 
+#define PAWN_PRIVATE 0x320 
+#define ROOT_COMPONENT 0x1B0
+#define RELATIVE_LOCATION 0x138
+#define PLAYER_STATE 0x2C8
+#define TEAM_INDEX 0x1259  
+#define GAME_STATE 0x188
+#define PLAYER_ARRAY 0x2C0
+#define MESH 0x328
+#define COMPONENT_TO_WORLD 0x1E0
+#define BONE_ARRAY 0x5A8
+#define BONE_ARRAY_CACHE 0x5B8
+#define LAST_SUBMIT_TIME 0x2E8
+#define LAST_SUBMIT_TIME_ON_SCREEN 0x2F0
+#define VELOCITY 0x180
+#define CURRENT_WEAPON 0xa80
+#define WEAPON_DATA 0x5E8 
+#define DISPLAY_NAME 0x40
+#define HABANERO_COMPONENT 0xa38
+#define RANKED_PROGRESS 0xd0
+#define PLATFORM 0x430
+#define WEAPON_NAME 0x40
+#define PLATFORM  0x430
+#define PERSISTENT_LEVEL 0x40
+#define ISDYING 0x718
+#define PERSISTENT_LEVEL 0x40
+#define GlobalAnimRateScale 0xa20
+#define bADSWhileNotOnGround 0x5869
