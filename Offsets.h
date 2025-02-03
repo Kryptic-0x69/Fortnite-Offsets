911e16f6eb4feb991d28c71e117d3cada8b095b7
Kryptic-0x69 <imsoscaredstop@gmail.com>
2025-02-03 01:01:57 -0500

    Update Offsets.h

--- Offsets.h
+++ Offsets.h
@@ -1,35 +1,33 @@
 // Fortnites Latest Update ++Fortnite+Release-33.30-CL-39435251-Windows
 // Date Of Update: 02/01/2025
 
-#define UWORLD 0x16FB0098
-#define GAME_INSTANCE 0x200
-#define LOCAL_PLAYERS 0x38
-#define PLAYER_CONTROLLER 0x30
-#define LOCAL_PAWN 0x350 
-#define PAWN_PRIVATE 0x320 
-#define ROOT_COMPONENT 0x1B0
-#define RELATIVE_LOCATION 0x138
-#define PLAYER_STATE 0x2C8
-#define TEAM_INDEX 0x1259  
-#define GAME_STATE 0x188
-#define PLAYER_ARRAY 0x2C0
-#define MESH 0x328
-#define COMPONENT_TO_WORLD 0x1E0
-#define BONE_ARRAY 0x5A8
-#define BONE_ARRAY_CACHE 0x5B8
-#define LAST_SUBMIT_TIME 0x2E8
-#define LAST_SUBMIT_TIME_ON_SCREEN 0x2F0
-#define VELOCITY 0x180
-#define CURRENT_WEAPON 0xa80
-#define WEAPON_DATA 0x5E8 
-#define DISPLAY_NAME 0x40
-#define HABANERO_COMPONENT 0xa38
-#define RANKED_PROGRESS 0xd0
-#define PLATFORM 0x430
-#define WEAPON_NAME 0x40
-#define PLATFORM  0x430
-#define PERSISTENT_LEVEL 0x40
-#define ISDYING 0x718
-#define PERSISTENT_LEVEL 0x40
-#define GlobalAnimRateScale 0xa20
-#define bADSWhileNotOnGround 0x5869
+uintptr_t UWORLD = 0x16FB0098;
+uintptr_t GAME_INSTANCE = 0x200;
+uintptr_t GAME_STATE = 0x188;
+uintptr_t MESH = 0x328;
+uintptr_t LOCAL_PLAYERS = 0x38;
+uintptr_t PLAYER_CONTROLLER = 0x30;
+uintptr_t LOCAL_PAWN = 0x350;
+uintptr_t PAWN_PRIVATE = 0x320;
+uintptr_t ROOT_COMPONENT = 0x1B0;
+uintptr_t PLAYER_STATE = 0x2C8;
+uintptr_t RELATIVE_LOCATION = 0x138;
+uintptr_t PLAYER_ARRAY = 0x2C0;
+uintptr_t COMPONENT_TO_WORLD = 0x1E0;
+uintptr_t BONE_ARRAY = 0x5A8;
+uintptr_t TEAM_INDEX = 0x1259;
+uintptr_t BONE_ARRAY_CACHE = 0x5B8;
+uintptr_t LAST_SUBMIT_TIME = 0x2E8;
+uintptr_t LAST_SUBMIT_TIME_ON_SCREEN = 0x2F0;
+uintptr_t VELOCITY = 0x180;
+uintptr_t CURRENT_WEAPON = 0xA80;
+uintptr_t WEAPON_DATA = 0x5E8;
+uintptr_t DISPLAY_NAME = 0x40;
+uintptr_t HABANERO_COMPONENT = 0xA38;
+uintptr_t RANKED_PROGRESS = 0xD0;
+uintptr_t PLATFORM = 0x430;
+uintptr_t WEAPON_NAME = 0x40;
+uintptr_t PERSISTENT_LEVEL = 0x40;
+uintptr_t ISDYING = 0x718;
+uintptr_t GlobalAnimRateScale = 0xA20;
+uintptr_t bADSWhileNotOnGround = 0x5869;
